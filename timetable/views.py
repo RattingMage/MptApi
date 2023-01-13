bf5870f861c26d4591158eff3c45e21350cfb25f
RattingMage <66022822+RattingMage@users.noreply.github.com>
2023-01-13 16:56:40 +0300

    I am loh

--- timetable/views.py
+++ timetable/views.py
@@ -61,50 +61,50 @@ class WeekView(APIView):
         
 
 class TimetableViews(APIView):
-    # def get(self, request):
-    #     number_group = request.GET.get("number_group")
-    #     with redis.Redis(host='redis', port=6379, db=0) as redis_client:
-    #         response = redis_client.json().get(f"timetable_{number_group}")
-    #
-    #     return JsonResponse(response, safe=False)
     def get(self, request):
-        html = utils.get_page('https://mpt.ru/studentu/raspisanie-zanyatiy/')
         number_group = request.GET.get("number_group")
-        group = html.find("a", string=number_group.replace("0", "О"))
-        href = group.get('href')
-        href = href[1::]
-        div = html.find('div', id=href)
-        tables = div.find_all('table')
-        JSON = []
-        for table in tables:
-            tds = table.find_all('td')
-            dct = {}
-            dct2 = {}
-            for i in range(len(tds)):
-                if i == 0:
-                    dct['number'] = tds[i].text
-                if i == 1:
-                    dct['subject'] = utils.check_zameny_sub(tds[i])
-                if i == 2:
-                    dct['teacher'] = utils.check_zameny_tech(tds[i])
-                    dct2['info'] = utils.find_str(table.find('h4').text.replace('\n', ''))
-                    dct2['timetable'] = dct
-                    JSON.append(dct2)
-                    dct = {}
-                    dct2 = {}
-                if i % 3 == 0:
-                    dct['number'] = tds[i].text
-                if i % 3 == 1:
-                    dct['subject'] = utils.check_zameny_sub(tds[i])
-                if i % 3 == 2 and i != 2:
-                    dct['teacher'] = utils.check_zameny_tech(tds[i])
-                    dct2['info'] = utils.find_str(table.find('h4').text.replace('\n', ''))
-                    dct2['timetable'] = dct
-                    JSON.append(dct2)
-                    dct = {}
-                    dct2 = {}
-        reJson = utils.refact_JSON(JSON)
-        return JsonResponse(reJson, safe=False)
+        with redis.Redis(host='redis', port=6379, db=0) as redis_client:
+            response = redis_client.json().get(f"timetable_{number_group.replace('0', 'О')}")
+
+        return JsonResponse(response, safe=False)
+    # def get(self, request):
+    #     html = utils.get_page('https://mpt.ru/studentu/raspisanie-zanyatiy/')
+    #     number_group = request.GET.get("number_group")
+    #     group = html.find("a", string=number_group.replace("0", "О"))
+    #     href = group.get('href')
+    #     href = href[1::]
+    #     div = html.find('div', id=href)
+    #     tables = div.find_all('table')
+    #     JSON = []
+    #     for table in tables:
+    #         tds = table.find_all('td')
+    #         dct = {}
+    #         dct2 = {}
+    #         for i in range(len(tds)):
+    #             if i == 0:
+    #                 dct['number'] = tds[i].text
+    #             if i == 1:
+    #                 dct['subject'] = utils.check_zameny_sub(tds[i])
+    #             if i == 2:
+    #                 dct['teacher'] = utils.check_zameny_tech(tds[i])
+    #                 dct2['info'] = utils.find_str(table.find('h4').text.replace('\n', ''))
+    #                 dct2['timetable'] = dct
+    #                 JSON.append(dct2)
+    #                 dct = {}
+    #                 dct2 = {}
+    #             if i % 3 == 0:
+    #                 dct['number'] = tds[i].text
+    #             if i % 3 == 1:
+    #                 dct['subject'] = utils.check_zameny_sub(tds[i])
+    #             if i % 3 == 2 and i != 2:
+    #                 dct['teacher'] = utils.check_zameny_tech(tds[i])
+    #                 dct2['info'] = utils.find_str(table.find('h4').text.replace('\n', ''))
+    #                 dct2['timetable'] = dct
+    #                 JSON.append(dct2)
+    #                 dct = {}
+    #                 dct2 = {}
+    #     reJson = utils.refact_JSON(JSON)
+    #     return JsonResponse(reJson, safe=False)
 
 
 class ReplacementView(APIView):
