19b7d6df734173a139d59f794c57ce3b23f14bbc
andry <mukhin2019@gmail.com>
2023-01-15 01:23:32 +0300

    fix CI v2

--- MptApi/settings.py
+++ MptApi/settings.py
@@ -129,7 +129,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REDIS_HOST = 'redis'
 REDIS_PORT = '6379'
-CELERY_BROKER_URL = f'redis://{REDIS_HOST}:{REDIS_PORT}/0'
+REDIS_PASS = "qwerty"
+CELERY_BROKER_URL = f'redis://{REDIS_PASS}@{REDIS_HOST}:{REDIS_PORT}/0'
 CELERY_BROKER_TRANSPORT_OPTIONS = {'visibility_timeout': 3600}
 CELERY_RESULT_BACKEND = f'redis://{REDIS_HOST}:{REDIS_PORT}/0'
 CELERY_ACCEPT_CONTENT = ['application/json']
